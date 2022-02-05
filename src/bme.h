3a24659c684fa597a6ac697e04f4a37448d9c7c1
deforde <daniel.forde001@gmail.com>
2022-02-05 17:01:30 +0200

    Clang-format and clang-tidy fixes (Barr C coding style).

--- src/bme.h
+++ src/bme.h
@@ -6,10 +6,10 @@
 
 bool bme_configure(int i2c_dev);
 
-bool bme_get_temp(int i2c_dev, float* t_degC, int32_t* t_fine);
+bool bme_get_temp(int i2c_dev, float *t_degC, int32_t *t_fine);
 
-bool bme_get_pressure(int i2c_dev, int32_t t_fine, float* p_kPa);
+bool bme_get_pressure(int i2c_dev, int32_t t_fine, float *p_kPa);
 
-bool bme_get_humidity(int i2c_dev, int32_t t_fine, float* humidity_pcnt);
+bool bme_get_humidity(int i2c_dev, int32_t t_fine, float *humidity_pcnt);
 
-#endif //BME_H
+#endif // BME_H
--- src/main.c
+++ src/main.c
@@ -1,38 +1,44 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 
 #include "bme.h"
 #include "i2c.h"
 
 #define I2C_DEV_NAME "/dev/i2c-2"
 
-int main()
+int
+main()
 {
     int i2c_dev = 0;
-    if(!i2c_init(I2C_DEV_NAME, &i2c_dev)) {
+    if (!i2c_init(I2C_DEV_NAME, &i2c_dev))
+    {
         return EXIT_FAILURE;
     }
 
-    if(!bme_configure(i2c_dev)) {
+    if (!bme_configure(i2c_dev))
+    {
         return EXIT_FAILURE;
     }
 
-    float t_degC = 0.0f;
+    float   t_degC = 0.0f;
     int32_t t_fine = 0;
-    if(!bme_get_temp(i2c_dev, &t_degC, &t_fine)) {
+    if (!bme_get_temp(i2c_dev, &t_degC, &t_fine))
+    {
         return EXIT_FAILURE;
     }
     printf("t_degC = %.2f degrees Celsius\n", t_degC / 100.0f);
 
     float p_kPa = 0.0f;
-    if(!bme_get_pressure(i2c_dev, t_fine, &p_kPa)) {
+    if (!bme_get_pressure(i2c_dev, t_fine, &p_kPa))
+    {
         return EXIT_FAILURE;
     }
     printf("p_kPa = %.2f kPa\n", p_kPa);
 
     float humidity_pcnt = 0.0f;
-    if(!bme_get_humidity(i2c_dev, t_fine, &humidity_pcnt)) {
+    if (!bme_get_humidity(i2c_dev, t_fine, &humidity_pcnt))
+    {
         return EXIT_FAILURE;
     }
     printf("humidity_pcnt = %.2f %%\n", humidity_pcnt);
